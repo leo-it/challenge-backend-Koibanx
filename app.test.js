e7af77b65bfb6a6ff0d7e4e6349250fe9e0689dc
leo <leo.database92@gmail.com>
2021-07-02 13:49:01 -1200

    added unit tests

--- app.test.js
+++ app.test.js
@@ -1,12 +1,70 @@
-
-const { expect } = require("@jest/globals");
 const isSolved = require("./app");
-  test('Tic tac toe ', () => {
+
+
+  test('returns 1 if x wins in a row ', () => {
     const solucion = isSolved([
       [1, 1, 1],
-      [0, 1, 2],
+      [0, 2, 2],
       [1, 1, 0],
     ])
     expect(solucion).toBe(1)
   })
- 
+  test('returns 2 if O wins in a row ', () => {
+    const solucion = isSolved([
+      [1, 2, 1],
+      [0, 1, 2],
+      [2, 2,2],
+    ])
+    expect(solucion).toBe(2)
+  })
+  test('returns 1 if x wins in a column ', () => {
+    const solucion = isSolved([
+      [1, 0, 1],
+      [1,2, 2],
+      [1, 1, 0],
+    ])
+    expect(solucion).toBe(1)
+  })
+  test('returns 2 if O wins in a column ', () => {
+    const solucion = isSolved([
+      [1, 2, 2],
+      [0, 1, 2],
+      [2,0,2],
+    ])
+    expect(solucion).toBe(2)
+  })
+
+  test('returns 1 if x wins in a diagonal ', () => {
+    const solucion = isSolved([
+      [1, 2, 1],
+      [0, 1, 2],
+      [1, 0, 0],
+    ])
+    expect(solucion).toBe(1)
+  })
+  test('returns 2 if O wins in a diagonal ', () => {
+    const solucion = isSolved([
+      [0, 2, 2],
+      [0, 2, 1],
+      [2, 0,2],
+    ])
+    expect(solucion).toBe(2)
+  })
+
+  test('return -1 if not yet finished ', () => {
+    const solucion = isSolved([
+      [2, 1, 2],
+      [1, 0, 1],
+      [2, 0,2],
+    ])
+    expect(solucion).toBe(-1)
+  })
+
+  test('return -1 if not yet finished ', () => {
+    const solucion = isSolved([
+      [2, 1, 1],
+      [1, 2, 2],
+      [2, 1,1],
+    ])
+    expect(solucion).toBe(0)
+  })
