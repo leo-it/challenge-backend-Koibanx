a689e0b9e71d04d490c28621990453d8c96fd677
leo <leo.database92@gmail.com>
2021-07-02 13:51:10 -1200

    added comments and solved some errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function isSolved(board) {
-  //fila ganadora
+  // returns 1 if x wins in a row
   for (i = 0; i < 3; i++) {
     if (
       board[i][0] === 1 &&
@@ -9,6 +9,8 @@ function isSolved(board) {
     ) {
       return 1;
     }
+    // returns 2 if O wins in a row
+
     if (
       board[i][0] === 2 &&
       board[i][0] === board[i][1] &&
@@ -18,7 +20,7 @@ function isSolved(board) {
       return 2;
     }
   }
-  //columna ganadora
+  // returns 1 if x wins in a column
   for (j = 0; j < 3; j++) {
     if (
       board[0][j] === 1 &&
@@ -28,6 +30,7 @@ function isSolved(board) {
     ) {
       return 1;
     }
+    // returns 2 if O wins in a column
     if (
       board[0][j] === 2 &&
       board[0][j] === board[1][j] &&
@@ -37,7 +40,7 @@ function isSolved(board) {
       return 2;
     }
   }
-  //diagonal ganadora
+  // returns 1 if X wins in a diagonal
   if (
     board[0][0] === 1 &&
     board[0][0] === board[1][1] &&
@@ -47,13 +50,15 @@ function isSolved(board) {
     return 1;
   }
   if (
-    board[0][0] === 1 &&
-    board[2][0] === board[1][1] &&
+    board[2][0] === 1 &&
+    board[0][2] === board[1][1] &&
     board[1][1] === board[0][2] &&
     board[2][0] !== 0
   ) {
     return 1;
   }
+  // returns 2 if O wins in a diagonal
+
   if (
     board[0][0] === 2 &&
     board[0][0] === board[1][1] &&
@@ -63,13 +68,14 @@ function isSolved(board) {
     return 2;
   }
   if (
-    board[0][0] === 2 &&
-    board[2][0] === board[1][1] &&
+    board[2][0] === 2 &&
+    board[0][2] === board[1][1] &&
     board[1][1] === board[0][2] &&
     board[2][0] !== 0
   ) {
     return 2;
   }
+  //return -1 if not yet finished
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
       if (board[i][j] === 0) {
@@ -77,15 +83,9 @@ function isSolved(board) {
       }
     }
   }
+  //return 0 if it's a cat's game
+
   return 0;
 }
-
-console.log(
-  isSolved([
-    [1, 1, 2],
-    [1, 2, 1],
-    [1, 2, 0],
-  ])
-);
 
 module.exports = isSolved;
