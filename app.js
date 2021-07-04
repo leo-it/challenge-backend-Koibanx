564524adc1461b24ea739c922db36374023107fb
Leonardo Sainz <leo.database92@gmail.com>
2021-07-03 12:47:21 -1200

    assigned numbers in constans

--- app.js
+++ app.js
@@ -1,91 +1,95 @@
 function isSolved(board) {
+  const X = 1,
+    O = 2,
+    emptyField = 0,
+    unfinished=-1;
   // returns 1 if x wins in a row
   for (i = 0; i < 3; i++) {
     if (
-      board[i][0] === 1 &&
+      board[i][0] === X &&
       board[i][0] === board[i][1] &&
       board[i][1] === board[i][2] &&
-      board[i][0] !== 0
+      board[i][0] !== emptyField
     ) {
-      return 1;
+      return X;
     }
     // returns 2 if O wins in a row
 
     if (
-      board[i][0] === 2 &&
+      board[i][0] === O &&
       board[i][0] === board[i][1] &&
       board[i][1] === board[i][2] &&
-      board[i][0] !== 0
+      board[i][0] !== emptyField
     ) {
-      return 2;
+      return O;
     }
   }
   // returns 1 if x wins in a column
   for (j = 0; j < 3; j++) {
     if (
-      board[0][j] === 1 &&
+      board[0][j] === X &&
       board[0][j] === board[1][j] &&
       board[1][j] === board[2][j] &&
-      board[0][j] !== 0
+      board[0][j] !== emptyField
     ) {
-      return 1;
+      return X;
     }
     // returns 2 if O wins in a column
     if (
-      board[0][j] === 2 &&
+      board[0][j] === O &&
       board[0][j] === board[1][j] &&
       board[1][j] === board[2][j] &&
-      board[0][j] !== 0
+      board[0][j] !== emptyField
     ) {
-      return 2;
+      return O;
     }
   }
   // returns 1 if X wins in a diagonal
   if (
-    board[0][0] === 1 &&
+    board[0][0] === X &&
     board[0][0] === board[1][1] &&
     board[1][1] === board[2][2] &&
-    board[0][0] !== 0
+    board[0][0] !== emptyField
   ) {
-    return 1;
+    return X;
   }
   if (
-    board[2][0] === 1 &&
+    board[2][0] === X &&
     board[0][2] === board[1][1] &&
     board[1][1] === board[0][2] &&
-    board[2][0] !== 0
+    board[2][0] !== emptyField
   ) {
-    return 1;
+    return X;
   }
   // returns 2 if O wins in a diagonal
 
   if (
-    board[0][0] === 2 &&
+    board[0][0] === O &&
     board[0][0] === board[1][1] &&
     board[1][1] === board[2][2] &&
-    board[0][0] !== 0
+    board[0][0] !== emptyField
   ) {
-    return 2;
+    return O;
   }
   if (
-    board[2][0] === 2 &&
+    board[2][0] === O &&
     board[0][2] === board[1][1] &&
     board[1][1] === board[0][2] &&
-    board[2][0] !== 0
+    board[2][0] !== emptyField
   ) {
-    return 2;
+    return O;
   }
   //return -1 if not yet finished
   for (i = 0; i < 3; i++) {
     for (j = 0; j < 3; j++) {
-      if (board[i][j] === 0) {
-        return -1;
+      if (board[i][j] === emptyField) {
+        return unfinished;
       }
     }
   }
   //return 0 if it's a cat's game
 
-  return 0;
+  return emptyField;
 }
 
 module.exports = isSolved;
--- app.test.js
+++ app.test.js
@@ -1,70 +1,73 @@
 const isSolved = require("./app");
+const X = 1,
+  O = 2,
+  emptyField = 0,
+  unfinished = -1;
 
+test("returns 1 if x wins in a row ", () => {
+  const solucion = isSolved([
+    [1, 1, 1],
+    [0, 2, 2],
+    [1, 1, 0],
+  ]);
+  expect(solucion).toBe(X);
+});
+test("returns 2 if O wins in a row ", () => {
+  const solucion = isSolved([
+    [1, 2, 1],
+    [0, 1, 2],
+    [2, 2, 2],
+  ]);
+  expect(solucion).toBe(O);
+});
+test("returns 1 if x wins in a column ", () => {
+  const solucion = isSolved([
+    [1, 0, 1],
+    [1, 2, 2],
+    [1, 1, 0],
+  ]);
+  expect(solucion).toBe(X);
+});
+test("returns 2 if O wins in a column ", () => {
+  const solucion = isSolved([
+    [1, 2, 2],
+    [0, 1, 2],
+    [2, 0, 2],
+  ]);
+  expect(solucion).toBe(O);
+});
 
-  test('returns 1 if x wins in a row ', () => {
-    const solucion = isSolved([
-      [1, 1, 1],
-      [0, 2, 2],
-      [1, 1, 0],
-    ])
-    expect(solucion).toBe(1)
-  })
-  test('returns 2 if O wins in a row ', () => {
-    const solucion = isSolved([
-      [1, 2, 1],
-      [0, 1, 2],
-      [2, 2,2],
-    ])
-    expect(solucion).toBe(2)
-  })
-  test('returns 1 if x wins in a column ', () => {
-    const solucion = isSolved([
-      [1, 0, 1],
-      [1,2, 2],
-      [1, 1, 0],
-    ])
-    expect(solucion).toBe(1)
-  })
-  test('returns 2 if O wins in a column ', () => {
-    const solucion = isSolved([
-      [1, 2, 2],
-      [0, 1, 2],
-      [2,0,2],
-    ])
-    expect(solucion).toBe(2)
-  })
+test("returns 1 if x wins in a diagonal ", () => {
+  const solucion = isSolved([
+    [1, 2, 1],
+    [0, 1, 2],
+    [1, 0, 0],
+  ]);
+  expect(solucion).toBe(X);
+});
+test("returns 2 if O wins in a diagonal ", () => {
+  const solucion = isSolved([
+    [0, 2, 2],
+    [0, 2, 1],
+    [2, 0, 2],
+  ]);
+  expect(solucion).toBe(O);
+});
 
-  test('returns 1 if x wins in a diagonal ', () => {
-    const solucion = isSolved([
-      [1, 2, 1],
-      [0, 1, 2],
-      [1, 0, 0],
-    ])
-    expect(solucion).toBe(1)
-  })
-  test('returns 2 if O wins in a diagonal ', () => {
-    const solucion = isSolved([
-      [0, 2, 2],
-      [0, 2, 1],
-      [2, 0,2],
-    ])
-    expect(solucion).toBe(2)
-  })
+test("return -1 if not yet finished ", () => {
+  const solucion = isSolved([
+    [2, 1, 2],
+    [1, 0, 1],
+    [2, 0, 2],
+  ]);
+  expect(solucion).toBe(unfinished);
+});
 
-  test('return -1 if not yet finished ', () => {
-    const solucion = isSolved([
-      [2, 1, 2],
-      [1, 0, 1],
-      [2, 0,2],
-    ])
-    expect(solucion).toBe(-1)
-  })
-
-  test('return -1 if not yet finished ', () => {
-    const solucion = isSolved([
-      [2, 1, 1],
-      [1, 2, 2],
-      [2, 1,1],
-    ])
-    expect(solucion).toBe(0)
-  })
+test("return 0 if its a cats game ", () => {
+  const solucion = isSolved([
+    [2, 1, 1],
+    [1, 2, 2],
+    [2, 1, 1],
+  ]);
+  expect(solucion).toBe(emptyField);
+});
