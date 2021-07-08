39bc13fe79e3cfdc7a40a22f7e8392ec5eeac95a
Leonardo Sainz <leo.database92@gmail.com>
2021-07-07 16:00:43 -1200

    terminado

--- app.js
+++ app.js
@@ -6,20 +6,14 @@ function isSolved(board) {
   // returns 1 if x wins in a row
   for (i = 0; i < 3; i++) {
     if (
-      board[i][0] === X &&
-      board[i][0] === board[i][1] &&
-      board[i][1] === board[i][2] &&
-      board[i][0] !== emptyField
+      board[i][0] === X && board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== emptyField
     ) {
       return X;
     }
     // returns 2 if O wins in a row
 
     if (
-      board[i][0] === O &&
-      board[i][0] === board[i][1] &&
-      board[i][1] === board[i][2] &&
-      board[i][0] !== emptyField
+      board[i][0] === O && board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== emptyField
     ) {
       return O;
     }
@@ -27,55 +21,37 @@ function isSolved(board) {
   // returns 1 if x wins in a column
   for (j = 0; j < 3; j++) {
     if (
-      board[0][j] === X &&
-      board[0][j] === board[1][j] &&
-      board[1][j] === board[2][j] &&
-      board[0][j] !== emptyField
+      board[0][j] === X && board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[0][j] !== emptyField
     ) {
       return X;
     }
     // returns 2 if O wins in a column
     if (
-      board[0][j] === O &&
-      board[0][j] === board[1][j] &&
-      board[1][j] === board[2][j] &&
-      board[0][j] !== emptyField
+      board[0][j] === O && board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[0][j] !== emptyField
     ) {
       return O;
     }
   }
   // returns 1 if X wins in a diagonal
   if (
-    board[0][0] === X &&
-    board[0][0] === board[1][1] &&
-    board[1][1] === board[2][2] &&
-    board[0][0] !== emptyField
+    board[0][0] === X && board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== emptyField
   ) {
     return X;
   }
   if (
-    board[2][0] === X &&
-    board[0][2] === board[1][1] &&
-    board[1][1] === board[0][2] &&
-    board[2][0] !== emptyField
+    board[2][0] === X && board[0][2] === board[1][1] && board[1][1] === board[0][2] && board[2][0] !== emptyField
   ) {
     return X;
   }
   // returns 2 if O wins in a diagonal
 
   if (
-    board[0][0] === O &&
-    board[0][0] === board[1][1] &&
-    board[1][1] === board[2][2] &&
-    board[0][0] !== emptyField
+    board[0][0] === O && board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== emptyField
   ) {
     return O;
   }
   if (
-    board[2][0] === O &&
-    board[0][2] === board[1][1] &&
-    board[1][1] === board[0][2] &&
-    board[2][0] !== emptyField
+    board[2][0] === O && board[0][2] === board[1][1] && board[1][1] === board[0][2] && board[2][0] !== emptyField
   ) {
     return O;
   }
@@ -88,7 +64,6 @@ function isSolved(board) {
     }
   }
   //return 0 if it's a cat's game
-
   return emptyField;
 }
 
