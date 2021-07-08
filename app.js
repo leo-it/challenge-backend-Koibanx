ad9f865f6596f221124d54f925aadda2342f9f62
Leonardo Sainz <leo.database92@gmail.com>
2021-07-07 16:21:12 -1200

    modifique el codigo para simplificarlo y abstraer un poco la logica en row, column y en diagonal

--- app.js
+++ app.js
@@ -3,57 +3,33 @@ function isSolved(board) {
     O = 2,
     emptyField = 0,
     unfinished=-1;
-  // returns 1 if x wins in a row
+  // returns 1 fi "x" wins or 2 if "o" wins in a row
   for (i = 0; i < 3; i++) {
     if (
-      board[i][0] === X && board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== emptyField
+      board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== emptyField
     ) {
-      return X;
-    }
-    // returns 2 if O wins in a row
-
-    if (
-      board[i][0] === O && board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== emptyField
-    ) {
-      return O;
+      return board[i][0];
     }
   }
-  // returns 1 if x wins in a column
+  // returns 1 fi "x" wins or 2 if "o" wins in a column
   for (j = 0; j < 3; j++) {
     if (
-      board[0][j] === X && board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[0][j] !== emptyField
+      board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[0][j] !== emptyField
     ) {
-      return X;
+      return board[0][j];
     }
-    // returns 2 if O wins in a column
-    if (
-      board[0][j] === O && board[0][j] === board[1][j] && board[1][j] === board[2][j] && board[0][j] !== emptyField
-    ) {
-      return O;
-    }
+    
   }
-  // returns 1 if X wins in a diagonal
+  // returns 1 fi "x" wins or 2 if "o" wins in a diagonal
   if (
-    board[0][0] === X && board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== emptyField
+    board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== emptyField
   ) {
-    return X;
+    return board[0][0];
   }
   if (
-    board[2][0] === X && board[0][2] === board[1][1] && board[1][1] === board[0][2] && board[2][0] !== emptyField
+     board[0][2] === board[1][1] && board[1][1] === board[0][2] && board[2][0] !== emptyField
   ) {
-    return X;
-  }
-  // returns 2 if O wins in a diagonal
-
-  if (
-    board[0][0] === O && board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== emptyField
-  ) {
-    return O;
-  }
-  if (
-    board[2][0] === O && board[0][2] === board[1][1] && board[1][1] === board[0][2] && board[2][0] !== emptyField
-  ) {
-    return O;
+    return board[0][2];
   }
   //return -1 if not yet finished
   for (i = 0; i < 3; i++) {
